81ac6a5e07fa5fcd1daec53b7ef37b327c46ee58
Guilherme Pereira <guilospanck@protonmail.com>
2025-09-27 17:04:36 +0100

    Add RPC to WS on the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,57 @@
 import { Elysia, t } from "elysia";
+import { RpcTarget, newWebSocketRpcSession } from "capnweb";
+
+// RPC common shared interfaces/types
+type UserInfo = {
+  id: string;
+  name: string;
+};
+
+interface AuthenticatedAPI {
+  getMyInfo(): UserInfo;
+}
+
+interface PublicAPI {
+  authenticate(token: string): AuthenticatedAPI;
+  getTodaysDate(): string;
+}
+
+class AuthenticatedAPIImpl extends RpcTarget implements AuthenticatedAPI {
+  getMyInfo(): UserInfo {
+    return {
+      id: "adfjkafj",
+      name: "Larry",
+    };
+  }
+}
+
+class PublicAPIImpl extends RpcTarget implements PublicAPI {
+  authenticate(token: string): AuthenticatedAPI {
+    if (token === "Tolkien") {
+      return new AuthenticatedAPIImpl();
+    }
+
+    throw new Error();
+  }
+  getTodaysDate(): string {
+    return Date.now().toLocaleString();
+  }
+}
 
 const app = new Elysia()
   .get("/", () => "Hello Elysia")
-  .ws("/rpc", {
+  // Normal WS without RPC
+  .ws("/ws", {
     body: t.Object({
       message: t.String(),
     }),
     query: t.Object({
       id: t.String(),
     }),
+    // On new WS connection
+    open(ws) {
+      console.info("New connection at /ws: ", ws.id);
+    },
     message(ws, { message }) {
       const { id } = ws.data.query;
       ws.send({
@@ -16,6 +59,14 @@ const app = new Elysia()
         message,
         time: Date.now(),
       });
+    },
+  })
+  // WS with RPC
+  .ws("/rpc", {
+    // On new WS connection
+    open(ws) {
+      console.info("New connection at /rpc: ", ws.id);
+      newWebSocketRpcSession(ws as any, new PublicAPIImpl());
     },
   })
   .listen(3000);
